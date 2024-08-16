f4889b5c023b6595506de3ef32b798705e933123
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-08-16 10:32:02 +0500

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -15,10 +15,11 @@ const Login = () => {
 
     return (
         <>
-            <form onSubmit={handleForm}>
-                <input required placeholder='Email' className='border' type="Email" />
-                <input required placeholder='Password' className='border' type="password" />
-                <button type='submit'>Admin Panel</button>
+            <h2 className='text-center  font-[700] mt-4'>Личный кабинет</h2>
+            <form className='flex flex-col items-center  w-full gap-3 mt-8' onSubmit={handleForm}>
+                <input  required placeholder='Email' className='w-[30%] py-1 px-3 border outline-none' type="text" />
+                <input required placeholder='Password' className='w-[30%] border py-1 px-3 outline-none' type="password" />
+                <button className='py-2 px-[185px] rounded-[40px] text-white bg-[rgb(255,138,30)]' type='submit'>Сохранить</button>
             </form>
 
         </>
