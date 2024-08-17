b9e1f4252d749f210113a9807fb3507214b83068
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-08-17 22:05:56 +0500

    login

--- src/pages/Admin/Manage.jsx
+++ src/pages/Admin/Manage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Manage = () => {
   return (
-    <div>Manage</div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque unde possimus similique est nemo fuga laudantium dolore sint sit necessitatibus expedita asperiores repellendus, deserunt corrupti nam tempore provident ab maiores nisi! Sunt corrupti commodi modi quisquam! Ducimus velit nobis necessitatibus!
+    </div>
   )
 }
 
